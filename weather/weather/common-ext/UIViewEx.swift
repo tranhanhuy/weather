89c5f87165fca9ad0b0d8a54c42d3931d8077863
Huy Tran <huy.tran@abcv.com>
2020-02-09 20:52:59 -0600

    Added UIViewExtension file

---
+++ weather/weather/common-ext/UIViewEx.swift
@@ -0,0 +1,16 @@
+// Created on 2/9/20.
+// Copyright (c) 2020 ABC Virtual Communications, Inc. All rights reserved.
+
+import UIKit
+
+extension UIView {
+  
+  func addContainerView(_ view: UIView) {
+    view.translatesAutoresizingMaskIntoConstraints = false
+    self.addSubview(view)
+    view.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+    view.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+    view.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+    view.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+  }
+}
