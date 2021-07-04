f9b49c2b1c37fdf9cfd9272dbd4e49eb84d4e254
tran.han.huy <tran.han.huy@sun-asterisk.com>
2021-07-04 15:34:29 +0700

    Remove set eo pipefail

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-set -eo pipefail
-
 cd weather
 
 xcodebuild -workspace weather.xcworkspace \
             -scheme weather \
             -destination platform=iOS\ Simulator,OS=14.4,name=iPhone\ 11 \
-            clean test | xcpretty
+            clean test
